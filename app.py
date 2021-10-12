0e8cbec3ad0fff91db7568045f9846dd97388ad0
nndcuong <nndcuong@gmail.com>
2021-10-12 22:48:08 +0700

    base flow

---
+++ app.py
@@ -0,0 +1,228 @@
+from flask import Flask, request, jsonify, url_for, send_from_directory
+from flask import render_template
+from markupsafe import escape
+import os
+from datetime import datetime, timedelta
+import argparse
+import pickle
+import time
+import numpy as np
+import json
+
+from database import get_latest_data, get_daily_latest_statistics
+
+app = Flask(__name__)
+
+@app.route("/hello", methods=["GET"])
+@app.route('/hello/<name>', methods=["GET"])
+def hello(name=None):
+    return render_template('hello.html', name=escape(name))
+
+@app.route("/old-home", methods=["GET"])
+def old_home():
+    return render_template('old_home.html',
+                            name='home',
+                            request=request,
+                            countries=countries,
+                            data_countries_current=data_countries_current,
+                            world_series=world_series,
+                            world_series_predict=world_series_predict,
+                            current_day=current_day)
+
+@app.route("/", methods=["GET"])
+@app.route("/<district>", methods=["GET"])
+def home(district="hcm"):
+    district = district.upper()
+    district = district.replace('-',' ')
+    
+    data = get_latest_data(district)
+    backup_dir = os.environ.get("BACKUP_DIR", "./backup")
+    backup_path = os.path.join(backup_dir,'backupdata.json')
+    if data is None:
+        with open(backup_path) as json_file:
+            data = json.load(json_file)
+    else:
+        with open(backup_path, 'w') as json_file:
+            json.dump(data, json_file)
+
+    summary = get_daily_latest_statistics()
+
+    # with open('sample.json') as f:
+    #     summary = json.load(f)
+    
+    districts = ['BINH CHANH', 'BINH TAN', 'BINH THANH', 'CAN GIO', 'CU CHI', 'GO VAP', 'HCM', 'HOC MON', 'NHA BE', 'PHU NHUAN'] + [f'QUAN {i}' for i in [1, 3, 4, 5, 6, 7, 8, 10, 11, 12]] + ['TAN BINH', 'TAN PHU', 'THU DUC']
+    districts.remove('HCM')
+    districts.sort()
+    districts.append('HCM')
+
+    return render_template('home.html',
+                            name = district,
+                            today = data['_id'],
+                            districts = districts,
+                            summary = summary['data'],
+                            data = data['data'],
+                            num_cols = [3 + (district == 'HCM'),1 + (district == 'HCM'),1]
+                            )
+
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if ('day-start' in request.form and 'day-end' in request.form):
+        day_start = datetime.strptime(request.form['day-start'], "%Y-%m-%d")
+        day_end = datetime.strptime(request.form['day-end'], "%Y-%m-%d")
+        number_of_days = (day_end - day_start).days
+        start_index = (day_start - datetime.strptime("2020-01-22", "%Y-%m-%d")).days
+
+        return render_template('predict.html',
+                                name='predict',
+                                data_series=np.array(data_series)[:,start_index:start_index+number_of_days].tolist(),
+                                start_day=request.form['day-start'],
+                                num_day=number_of_days,
+                                date_series=date_series[start_index:start_index+number_of_days])
+
+    return render_template('predict.html',
+                            name='predict',
+                            data_series=data_series,
+                            start_day='2020-01-22',
+                            num_day=len(data_series[0]),
+                            date_series=date_series)
+
+@app.route("/whitepaper", methods=["GET"])
+def whitepaper():
+    return render_template('whitepaper.html',
+                            name='whitepaper')
+
+@app.route("/donate", methods=["GET"])
+def donate():
+    return render_template('donate.html',
+                            name='donate')
+
+@app.route("/achievements", methods=["GET"])
+def achievements():
+    return render_template('achievements.html',
+                            name='achievements')
+
+@app.route("/acknowledgement", methods=["GET"])
+def acknowledgement():
+    return render_template('acknowledgement.html',
+                            name='acknowledgement')
+
+@app.route("/contact", methods=["GET"])
+def contact():
+    return render_template('contact.html',
+                            name='contact')
+
+@app.route("/googlede2ce4a4cee74360.html", methods=["GET"])
+def googlede2ce4a4cee74360():
+    return render_template('googlede2ce4a4cee74360.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico')
+
+def load_predict_result(data_folder):
+    global countries
+    global world_series
+    global world_series_predict
+    global data_series
+    global date_series
+    global current_day
+    global data_countries_current
+
+    countries              = pickle.load(open(data_folder + "/countries.pkl", 'rb'))
+    world_series           = pickle.load(open(data_folder + "/world_series.pkl", 'rb'))
+    world_series_predict   = pickle.load(open(data_folder + "/world_series_predict.pkl", 'rb'))
+    data_series            = pickle.load(open(data_folder + "/data_series.pkl", 'rb'))
+    date_series            = pickle.load(open(data_folder + "/date_series.pkl", 'rb'))
+    current_day            = pickle.load(open(data_folder + "/current_day.pkl", 'rb'))
+    data_countries_current = pickle.load(open(data_folder + "/data_countries_current.pkl", 'rb'))
+
+def init(run_predict=False, data_folder="./"):
+    if not run_predict:
+        load_predict_result(data_folder)
+        return
+
+    from data_utils import DataLoader
+    from becaked import BeCakedModel
+    data_loader = DataLoader()
+    becaked_model = BeCakedModel()
+
+    global countries
+    countries = data_loader.get_countries()
+    pickle.dump(countries, open(data_folder + "/countries.pkl", "wb"))
+
+    global world_series # For home page
+    world_series = [x.tolist()[-30:] for x in data_loader.get_data_world_series()]
+    world_series.insert(1, [x[0] - x[1] - x[2] for x in zip(world_series[0], world_series[1], world_series[2])])
+    pickle.dump(world_series, open(data_folder + "/world_series.pkl", "wb"))
+
+    global world_series_predict
+    world_series_predict = [[], [], [], []]
+
+    for _ in range(30):
+        world_series[0].append(None)
+        world_series[1].append(None)
+        world_series[2].append(None)
+        world_series[3].append(None)
+
+    global data_series
+    world_series_temp = [x.tolist() for x in data_loader.get_data_world_series()]
+    data_series = [[], [], [], []]
+
+    for i in range(10):
+        data_series[0].append(world_series_temp[0][i])
+        data_series[1].append(world_series_temp[0][i] - world_series_temp[1][i] - world_series_temp[2][i])
+        data_series[2].append(world_series_temp[1][i])
+        data_series[3].append(world_series_temp[2][i])
+
+    for i in range(len(world_series_temp[0]) - 10):
+        res = becaked_model.predict([world_series_temp[0][i:i+10], world_series_temp[1][i:i+10], world_series_temp[2][i:i+10]])[0][-1]
+        data_series[0].append(int(res[1] + res[2] + res[3]))
+        data_series[1].append(int(res[1]))
+        data_series[2].append(int(res[2]))
+        data_series[3].append(int(res[3]))
+
+    world_series_predict[0] = data_series[0][len(world_series_temp[0])-30:len(world_series_temp[0])+30]
+    world_series_predict[1] = data_series[1][len(world_series_temp[0])-30:len(world_series_temp[0])+30]
+    world_series_predict[2] = data_series[2][len(world_series_temp[0])-30:len(world_series_temp[0])+30]
+    world_series_predict[3] = data_series[3][len(world_series_temp[0])-30:len(world_series_temp[0])+30]
+
+    pickle.dump(data_series, open(data_folder + "/data_series.pkl", "wb"))
+    pickle.dump(world_series_predict, open(data_folder + "/world_series_predict.pkl", "wb"))
+
+    sd = datetime.strptime("2020-01-22", "%Y-%m-%d")
+    global date_series
+    date_series = []
+    for i in range(len(data_series[0])):
+        date_series.append((sd+timedelta(days=i)).strftime("%d / %m / %Y"))
+    pickle.dump(date_series, open(data_folder + "/date_series.pkl", "wb"))
+
+    global current_day
+    current_day = data_loader.get_current_day()
+    pickle.dump(current_day, open(data_folder + "/current_day.pkl", "wb"))
+
+    global data_countries_current
+    data_countries_current = data_loader.get_data_countries_current()
+    data_countries_current.insert(1, {country:abs(data_countries_current[0][country] - data_countries_current[1][country] - data_countries_current[2][country])
+                                    if country in data_countries_current[1] else abs(data_countries_current[0][country] - data_countries_current[2][country])
+                                    for country in countries[0]})
+    pickle.dump(data_countries_current, open(data_folder + "/data_countries_current.pkl", "wb"))
+
+def update_data():
+    print("Updating data!")
+    os.system("rm -rf COVID-19/csse_covid_19_data/csse_covid_19_time_series")
+    os.system("svn checkout --force https://github.com/CSSEGISandData/COVID-19/trunk/csse_covid_19_data/csse_covid_19_time_series COVID-19/csse_covid_19_data/csse_covid_19_time_series")
+    time.sleep(30)
+    init(run_predict=True)
+
+def main():
+    run_init = bool(os.environ.get("INIT_DATA", True))
+    data_dir = str(os.environ.get("DATA_DIR", "./web_data"))
+    #init(run_init, data_dir)
+
+    return app
+
+if __name__ == "__main__":
+    app = main()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='127.0.0.1', port=port)
---
+++ database.py
@@ -0,0 +1,145 @@
+from pymongo import MongoClient
+import glob
+import os
+import pandas as pd
+import shutil
+import sys
+from datetime import datetime as dt, timedelta
+
+def remove_v2_path(folder_name = 'data'):
+    all_csv_paths = glob.glob(f'{folder_name}/*/v2/*/*/*.csv')
+    for path in all_csv_paths:
+        new_path = path.replace('/v2', '')
+        new_dir = new_path.rsplit('/', 1)[0]
+        if not os.path.exists(new_dir):
+            os.system(f'mkdir -p "{new_dir}"')
+        os.replace(path, new_path)
+
+def prepare_all_data(folder_name = 'data'):
+    all_csv_paths = glob.glob(f'{folder_name}/*/*/*/*.csv')
+    districts = ['BINH CHANH', 'BINH TAN', 'BINH THANH', 'CAN GIO', 'CU CHI', 'GO VAP', 'HCM', 'HOC MON', 'NHA BE', 'PHU NHUAN'] + [f'QUAN {i}' for i in [1, 3, 4, 5, 6, 7, 8, 10, 11, 12]] + ['TAN BINH', 'TAN PHU', 'THU DUC']
+    data = {}
+    for district in districts:
+        paths = list(filter(lambda x: district in x, all_csv_paths))
+        data[district] = {}
+        for path in paths:
+            coms = path.split(os.path.sep)
+            date = coms[1]
+            case = coms[-2]
+            if os.stat(path).st_size == 0: continue
+            df = pd.read_csv(path)
+            name = coms[-1]
+            if 'infectious' in name:
+                compartment = 'I'
+            elif 'recovered' in name:
+                compartment = 'R'
+            elif 'deceased' in name:
+                compartment = 'D'
+            elif 'critical' in name or 'ecmo' in name:
+                compartment = 'C'
+            if date not in data[district]:
+                data[district][date] = {}
+                data[district][date][compartment] = {}
+                data[district][date][compartment][case] = df['Predict'].values.tolist()
+                if 'Real' in df.columns:
+                    data[district][date][compartment]['real'] = df['Real'].dropna(axis = 0).values.tolist()
+                data[district][date]['dates'] = df['Date'].values.tolist()
+            else:
+                if compartment not in data[district][date]:
+                    data[district][date][compartment] = {}
+                if 'Real' in df.columns and 'real' not in data[district][date][compartment]:
+                    data[district][date][compartment]['real'] = df['Real'].dropna(axis = 0).values.tolist()
+                data[district][date][compartment][case] = df['Predict'].values.tolist()
+    return data
+def convert_into_mongo_format(data):
+    mongo_data = {}
+    for name, value in data.items():
+        mongo_data[name] = []
+        for date, _data in value.items():
+            mongo_data[name].append({'_id': date, 'data': _data})
+    return mongo_data
+
+def insert_new_data(db, folder_name):
+    data = prepare_all_data(folder_name)
+    mongo_data = convert_into_mongo_format(data)
+    for name, value in mongo_data.items():
+        if len(value) == 0:
+            print(f'MISSED {name}')
+            continue
+        db[name].insert_many(value)
+
+def query_data(db, district, date):
+    return db[district].find_one({"_id": date})
+
+def get_latest_data(district='HCM', skip_missing = False):
+    try:
+        client = MongoClient("mongodb+srv://thesisbecaked:thesisbecaked@thesis.cojlj.mongodb.net")
+        db = client['daily-data']
+        latest_date = db.auxiliary.find_one({"type": "latest_date"}, {"latest_date": 1})['latest_date']
+        if skip_missing:
+            return query_data(db, district, latest_date)
+        is_exist = db[district].find_one({"_id": latest_date}, {"_id": 1})
+        if is_exist is not None:
+            return query_data(db, district, latest_date)
+        last_date = list(db[district].find({}, {"_id": 1}).sort("_id", -1))[0]['_id']
+        client.close()
+        return query_data(db, district, last_date)
+    except:
+        return None
+
+def get_daily_latest_statistics():
+    client = MongoClient("mongodb+srv://thesisbecaked:thesisbecaked@thesis.cojlj.mongodb.net")
+    db = client['daily-data']
+    latest_date = db.auxiliary.find_one({"type": "latest_date"}, {"latest_date": 1})['latest_date']
+    tmp = latest_date.split('.')
+    month, date = int(tmp[0]), int(tmp[1])
+    pre_date = dt.strptime(f'{date}/{month}/21', '%d/%m/%y') - timedelta(days = 1)
+    curr_cum_date = f'{pre_date.month}.{pre_date.day}'
+    data = db.cum_data.find_one({"_id": curr_cum_date})
+    skips = ['cum_data', 'auxiliary']
+    districts = db.list_collection_names()
+    rv = {}
+    rv['date'] = curr_cum_date
+    rv['data'] = {}
+    for district in districts:
+        if district in skips: continue
+        result = db[district].find_one({"_id": latest_date})
+        if result is None: continue
+        result = result['data']
+        try:
+            D = result['D']['real'][-1] - result['D']['real'][-2]
+        except:
+            D = None
+        try:
+            R = result['R']['real'][-1] - result['R']['real'][-2]
+        except:
+            R = None
+        try:
+            I = result['I']['real'][-1]
+        except:
+            I = None
+        acc_I = data['I'].get(district, None)
+        acc_R = data['R'].get(district, None)
+        acc_D = data['D'].get(district, None)
+        rv['data'][district] = {
+            'I': {'New': I, 'Total': acc_I},
+            'R': {'New': R, 'Total': acc_R},
+            'D': {'New': D, 'Total': acc_D}
+        }
+    client.close()
+    return rv
+
+#test
+if __name__ == '__main__':
+    # if len(sys.argv) > 1:
+    #     folder_name = sys.argv[1]
+    #     remove_v2_path(folder_name)
+
+    # client = MongoClient("mongodb+srv://thesisbecaked:thesisbecaked@thesis.cojlj.mongodb.net")
+    # # client = MongoClient('mongodb://localhost:27017/')
+    # db = client['daily-data']
+    # if len(sys.argv) > 1:
+    #     insert_new_data(db, folder_name)
+    # print(query_data(db, "HCM", "10.12"))
+    # client.close()
+    print(get_latest_data('QUAN 3'))
