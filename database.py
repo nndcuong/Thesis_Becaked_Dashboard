c9ac176ef9beff288a8c9f735592916e95ffcc9d
minhngh <minh.nghminh@gmail.com>
2021-10-13 22:37:53 +0700

    fix daily statistics and add db_write

--- database.py
+++ database.py
@@ -6,67 +6,6 @@ import shutil
 import sys
 from datetime import datetime as dt, timedelta
 
-def remove_v2_path(folder_name = 'data'):
-    all_csv_paths = glob.glob(f'{folder_name}/*/v2/*/*/*.csv')
-    for path in all_csv_paths:
-        new_path = path.replace('/v2', '')
-        new_dir = new_path.rsplit('/', 1)[0]
-        if not os.path.exists(new_dir):
-            os.system(f'mkdir -p "{new_dir}"')
-        os.replace(path, new_path)
-
-def prepare_all_data(folder_name = 'data'):
-    all_csv_paths = glob.glob(f'{folder_name}/*/*/*/*.csv')
-    districts = ['BINH CHANH', 'BINH TAN', 'BINH THANH', 'CAN GIO', 'CU CHI', 'GO VAP', 'HCM', 'HOC MON', 'NHA BE', 'PHU NHUAN'] + [f'QUAN {i}' for i in [1, 3, 4, 5, 6, 7, 8, 10, 11, 12]] + ['TAN BINH', 'TAN PHU', 'THU DUC']
-    data = {}
-    for district in districts:
-        paths = list(filter(lambda x: district in x, all_csv_paths))
-        data[district] = {}
-        for path in paths:
-            coms = path.split(os.path.sep)
-            date = coms[1]
-            case = coms[-2]
-            if os.stat(path).st_size == 0: continue
-            df = pd.read_csv(path)
-            name = coms[-1]
-            if 'infectious' in name:
-                compartment = 'I'
-            elif 'recovered' in name:
-                compartment = 'R'
-            elif 'deceased' in name:
-                compartment = 'D'
-            elif 'critical' in name or 'ecmo' in name:
-                compartment = 'C'
-            if date not in data[district]:
-                data[district][date] = {}
-                data[district][date][compartment] = {}
-                data[district][date][compartment][case] = df['Predict'].values.tolist()
-                if 'Real' in df.columns:
-                    data[district][date][compartment]['real'] = df['Real'].dropna(axis = 0).values.tolist()
-                data[district][date]['dates'] = df['Date'].values.tolist()
-            else:
-                if compartment not in data[district][date]:
-                    data[district][date][compartment] = {}
-                if 'Real' in df.columns and 'real' not in data[district][date][compartment]:
-                    data[district][date][compartment]['real'] = df['Real'].dropna(axis = 0).values.tolist()
-                data[district][date][compartment][case] = df['Predict'].values.tolist()
-    return data
-def convert_into_mongo_format(data):
-    mongo_data = {}
-    for name, value in data.items():
-        mongo_data[name] = []
-        for date, _data in value.items():
-            mongo_data[name].append({'_id': date, 'data': _data})
-    return mongo_data
-
-def insert_new_data(db, folder_name):
-    data = prepare_all_data(folder_name)
-    mongo_data = convert_into_mongo_format(data)
-    for name, value in mongo_data.items():
-        if len(value) == 0:
-            print(f'MISSED {name}')
-            continue
-        db[name].insert_many(value)
 
 def query_data(db, district, date):
     return db[district].find_one({"_id": date})
@@ -82,8 +21,10 @@ def get_latest_data(district='HCM', skip_missing = False):
         if is_exist is not None:
             return query_data(db, district, latest_date)
         last_date = list(db[district].find({}, {"_id": 1}).sort("_id", -1))[0]['_id']
+        
+        data = query_data(db, district, last_date)
         client.close()
-        return query_data(db, district, last_date)
+        return data
     except:
         return None
 
@@ -104,20 +45,21 @@ def get_daily_latest_statistics():
     for district in districts:
         if district in skips: continue
         result = db[district].find_one({"_id": latest_date})
-        if result is None: continue
-        result = result['data']
-        try:
-            D = result['D']['real'][-1] - result['D']['real'][-2]
-        except:
-            D = None
-        try:
-            R = result['R']['real'][-1] - result['R']['real'][-2]
-        except:
-            R = None
-        try:
-            I = result['I']['real'][-1]
-        except:
-            I = None
+        I, R, D = None, None, None
+        if result is not None:
+            result = result['data']
+            try:
+                D = result['D']['real'][-1] - result['D']['real'][-2]
+            except:
+                pass
+            try:
+                R = result['R']['real'][-1] - result['R']['real'][-2]
+            except:
+                pass
+            try:
+                I = result['I']['real'][-1]
+            except:
+                pass
         acc_I = data['I'].get(district, None)
         acc_R = data['R'].get(district, None)
         acc_D = data['D'].get(district, None)
